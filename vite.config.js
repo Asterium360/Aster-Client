d26573a189939ca896c5447e957907092b851dd2
Larysa Ambartsumian <hi.ambalari@gmail.com>
2025-10-02 09:45:33 +0200

    added new photos

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-
-  plugins: [react(), tailwindcss()],
-
   plugins: [react()],
   css: {
     postcss: './postcss.config.js'
   },
-
-  test:{
-    globals:true,
+  test: {
+    globals: true,
     environment: 'jsdom'
   }
 })
