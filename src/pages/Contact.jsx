5b9a0e0999f7143c8261c9972f3f49f23817b54c
Sofiareyes12 <121201sofiareyes@gmail.com>
2025-10-09 12:06:07 +0200

    button.test.jsx file created to testing button component

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -193,7 +193,7 @@ const Contact = () => {
                     </>
                   ) : (
                     <>
-                      <span>Enviar mensaje</span>
+                      <span>Enviar mensaje </span>
                       <svg className="w-4 h-4" viewBox="0 0 24 24" fill="none">
                         <path d="M5 12h14M12 5l7 7-7 7" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                       </svg>
